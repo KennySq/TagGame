65b9731e46fbc105a875743aababe590dddb7dfa
JUNG JISANG <ashuatz@gmail.com>
2022-01-21 20:04:04 +0900

    add photon network source. modified photon server setting(appid).

---
+++ Assets/Scripts/Network/PhotonManager.cs
@@ -0,0 +1,304 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Realtime;
+using Photon.Pun;
+using System;
+using System.Linq;
+using Utils;
+
+namespace TagGame.Photon
+{
+    public class PhotonSingleton<T> : MonoBehaviourPunCallbacks, IPunObservable where T : PhotonSingleton<T>
+    {
+        protected static T _instance;
+
+        private static object _lock = new object();
+
+        public static T Instance
+        {
+            get
+            {
+                if (applicationIsQuitting)
+                {
+                    Debug.LogWarning("[Singleton] Instance '" + typeof(T) +
+                        "' already destroyed on application quit." +
+                        " Won't create again - returning null.");
+                    return null;
+                }
+
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = (T)FindObjectOfType(typeof(T));
+
+                        if (FindObjectsOfType(typeof(T)).Length > 1)
+                        {
+                            Debug.LogError("[Singleton] Something went really wrong " +
+                                " - there should never be more than 1 singleton!" +
+                                " Reopening the scene might fix it.");
+                            return _instance;
+                        }
+
+                        if (_instance == null)
+                        {
+                            GameObject singleton = new GameObject();
+                            _instance = singleton.AddComponent<T>();
+                            singleton.name = "(singleton) " + typeof(T).ToString();
+
+                            DontDestroyOnLoad(singleton);
+                            Debug.Log("[Singleton] An instance of " + typeof(T) +
+                                " is needed in the scene, so '" + singleton +
+                                "' was created with DontDestroyOnLoad.");
+                        }
+                        else
+                        {
+                            Debug.Log("[Singleton] Using instance already created: " +
+                                _instance.gameObject.name);
+                        }
+                    }
+
+                    return _instance;
+                }
+            }
+        }
+
+        private static event Action<T> onInitialized;
+        public static event Action<T> OnInitialized
+        {
+            add
+            {
+                if (_instance != null)
+                {
+                    value?.Invoke(_instance);
+                    return;
+                }
+
+                onInitialized += value;
+            }
+            remove
+            {
+                onInitialized -= value;
+            }
+        }
+        public static bool isInitialized { get; protected set; }
+        private static bool applicationIsQuitting = false;
+        /// <summary>
+        /// When Unity quits, it destroys objects in a random order.
+        /// In principle, a Singleton is only destroyed when application quits.
+        /// If any script calls Instance after it have been destroyed, 
+        ///   it will create a buggy ghost object that will stay on the Editor scene
+        ///   even after stopping playing the Application. Really bad!
+        /// So, this was made to be sure we're not creating that buggy ghost object.
+        /// </summary>
+        public virtual void OnDestroy()
+        {
+            if (_instance == this)
+            {
+                applicationIsQuitting = true;
+            }
+        }
+
+        public virtual void OnApplicationQuit()
+        {
+            applicationIsQuitting = true;
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+
+                //invoke added handler
+                isInitialized = true;
+
+                onInitialized?.Invoke(_instance);
+                onInitialized = null;
+
+            }
+            else
+            {
+                DestroyImmediate(gameObject);
+            }
+        }
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+
+        }
+
+        public static bool TryGetInstance(out T instance)
+        {
+            instance = _instance;
+            return instance != null;
+        }
+    }
+
+    public partial class PhotonManager : PhotonSingleton<PhotonManager>
+    {
+        public Notifier<int> ConnectedPlayerCount { get; private set; } = new Notifier<int>();
+        public NotifierClass<string> ConnectionState { get; private set; } = new NotifierClass<string>();
+
+
+        public readonly NotifierClass<string> CurrenetRoomName = new NotifierClass<string>();
+        public readonly NotifierClass<string> LastRoomName = new NotifierClass<string>();
+
+        public event Action<long> OnPlayerDisconnected;
+
+        private void Start()
+        {
+            PhotonNetwork.SerializationRate = 10;
+            PhotonNetwork.SendRate = 20;
+
+            var photonView = PhotonView.Get(this);
+            photonView = gameObject.AddComponent<PhotonView>();
+            photonView.ObservedComponents = new List<Component>(0);
+            photonView.ObservedComponents.Add(this);
+            photonView.ViewID = 1;
+            //PhotonNetwork.AddCallbackTarget(this);
+
+            PhotonNetwork.MinimalTimeScaleToDispatchInFixedUpdate = 1f;
+
+            ConnectionState.OnDataChanged += ConnectionState_OnDataChanged;
+        }
+
+
+        private void ConnectionState_OnDataChanged(string obj)
+        {
+            Debug.Log("ConnectionState : " + obj);
+        }
+
+
+        public void StartConnect()
+        {
+            PhotonNetwork.LocalPlayer.NickName = Guid.NewGuid().ToString();
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        public override void OnRoomListUpdate(List<RoomInfo> roomList)
+        {
+            roomList.RemoveAll((info) => info.Name.Equals(LastRoomName.CurrentData));
+
+            foreach (var room in roomList)
+            {
+                if (room.PlayerCount == 0)
+                    continue;
+                if (room.PlayerCount == room.MaxPlayers)
+                    continue;
+                if (!room.IsOpen)
+                    continue;
+                if (!room.IsVisible)
+                    continue;
+
+                PhotonNetwork.JoinRoom(room.Name);
+                return;
+            }
+
+
+            CreateRoom();
+            LastRoomName.CurrentData = string.Empty;
+        }
+
+        private void CreateRoom()
+        {
+            RoomOptions roomOptions = new RoomOptions
+            {
+                PublishUserId = true,
+                MaxPlayers = 8
+            };
+
+            PhotonNetwork.CreateRoom("Lobby" + Time.time, roomOptions);
+        }
+
+        public void StartDisconnect()
+        {
+            if (PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.Disconnect();
+            }
+        }
+
+        private void CheckMatchableCondition()
+        {
+            int playerCount = PhotonNetwork.CurrentRoom.Players.Count;
+
+            foreach (var player in PhotonNetwork.CurrentRoom.Players.OrderBy(i => i.Value.ActorNumber))
+            {
+                //add playerinfo here
+            }
+
+
+            switch (playerCount)
+            {
+                case 1:
+                {
+                    break;
+                }
+
+                case 2:
+                {
+                    break;
+                }
+
+                default:
+                    break;
+            }
+
+            ConnectedPlayerCount.CurrentData = playerCount;
+        }
+
+        //callbacks
+        public override void OnConnectedToMaster()
+        {
+            PhotonNetwork.JoinLobby();
+            ConnectionState.CurrentData = "StartConnect";
+        }
+
+        public override void OnJoinedLobby()
+        {
+            ConnectionState.CurrentData = "StartConnect";
+            CurrenetRoomName.CurrentData = string.Empty;
+        }
+
+        public override void OnJoinedRoom()
+        {
+            CurrenetRoomName.CurrentData = PhotonNetwork.CurrentRoom.Name;
+            LastRoomName.CurrentData = PhotonNetwork.CurrentRoom.Name;
+            ConnectionState.CurrentData = "JoinedRoom";
+            CheckMatchableCondition();
+        }
+
+        public override void OnPlayerEnteredRoom(Player newPlayer)
+        {
+            CheckMatchableCondition();
+        }
+        public override void OnPlayerLeftRoom(Player otherPlayer)
+        {
+            CheckMatchableCondition();
+            if (long.TryParse(otherPlayer.NickName, out var uuid))
+            {
+                OnPlayerDisconnected?.Invoke(uuid);
+            }
+        }
+
+        public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+        {
+            CheckMatchableCondition();
+        }
+
+        public override void OnJoinRoomFailed(short returnCode, string message)
+        {
+            CurrenetRoomName.CurrentData = string.Empty;
+            PhotonNetwork.JoinLobby();
+        }
+
+        public bool isMasterClient()
+        {
+            return PhotonNetwork.LocalPlayer.IsMasterClient;
+        }
+    }
+}
---
+++ Assets/Scripts/Network/PhotonManagerPart.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Realtime;
+using Photon.Pun;
+using System;
+using System.Linq;
+using Utils;
+using TagGame.Photon;
+
+namespace TagGame.Photon
+{
+    [Serializable]
+    public class Packet
+    {
+        public long UUID;
+        public int time_stamp;
+
+        public Packet()
+        {
+            UUID = PlayerUUID.UUID;
+            time_stamp = PhotonManager.GetTimeStamp;
+        }
+
+        public virtual Packet Serialize()
+        {
+            return this;
+        }
+
+        public virtual Packet DeSerialize()
+        {
+            return this;
+        }
+    }
+
+
+    [Serializable]
+    public class TrackedPosePacket : Packet
+    {
+        public Vector3 pos;
+        public Vector3 velocity;
+
+        public Quaternion rot;
+        public Vector3 angularVelocity;
+    }
+
+
+
+    public partial class PhotonManager : PhotonSingleton<PhotonManager>
+    {
+
+        public event Action<TrackedPosePacket> OnTrackedPoseMessageReceive;
+
+        private const RpcTarget SendTarget = RpcTarget.Others;
+
+        public static float RoundTripTime { get => PhotonNetwork.NetworkingClient.LoadBalancingPeer.RoundTripTime; }
+
+
+        public static int GetTimeStamp
+        {
+            get
+            {
+                return PhotonNetwork.ServerTimestamp;
+            }
+        }
+
+        public static void FetchTime()
+        {
+            PhotonNetwork.FetchServerTimestamp();
+        }
+
+
+        //implement serialization here
+        private static string SerializeData<T>(in T packet) where T : Packet
+        {
+            return JsonUtility.ToJson(packet.Serialize());
+        }
+
+        private static T DeserializeData<T>(in string data) where T : Packet
+        {
+            var packet = JsonUtility.FromJson<T>(data);
+            return packet.DeSerialize() as T;
+        }
+
+        private static bool isValid
+        {
+            get =>
+                isInitialized &&
+                PhotonNetwork.IsConnected &&
+                Instance.photonView != null &&
+                Instance.photonView.isActiveAndEnabled &&
+                PhotonNetwork.InRoom &&
+                PhotonNetwork.CurrentRoom.PlayerCount > 1;
+        }
+
+
+        //RPC Send/Receive
+        //Define Static class for connection test
+        public static void SendTrackedPoseData(in TrackedPosePacket data)
+        {
+            if (!isValid)
+                return;
+
+            Instance.photonView.RPC("ReceiveTrackedPoseData", SendTarget, SerializeData(data));
+        }
+
+        [PunRPC]
+        public void ReceiveTrackedPoseData(string data)
+        {
+            var packet = DeserializeData<TrackedPosePacket>(data);
+            OnTrackedPoseMessageReceive?.Invoke(packet);
+        }
+
+    }
+
+
+}
