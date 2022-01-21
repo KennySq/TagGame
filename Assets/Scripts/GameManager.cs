8e041fe56c005aac13f9cb530c25bd313a9adf59
KennySq <odessa5d4@gmail.com>
2022-01-21 11:37:14 +0900

    Update network package

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public string IPAddress;
+    public Button ConnectButton;
+    
+    void Start()
+    {
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool Connect(string ip)
+    {
+        
+
+        return true;
+    }
+}
